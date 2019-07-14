fe0fcb103b23f6987a9b06dfe0d82b069b252df6
Shaikh, Noor Mohammed <ShaikhN@autonation.com>
2019-07-14 12:15:29 -0400

    Confirming the Last Operation

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -62,6 +62,7 @@ namespace OdeToFood.Pages.Restaurants
                 this.restaurantData.Add(Restaurant);
             }
             restaurantData.Commit();
+            TempData["Message"] = "Restaurant saved!!";
             return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
         }
     }
